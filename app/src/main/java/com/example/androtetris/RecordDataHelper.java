05814e0c2d54a1dbf3f7a584debf6fb8d7837d73
Andrey S. Ionisyan <anserion@gmail.com>
2021-06-14 17:48:43 +0300

    comments and apache license added

--- app/src/main/java/com/example/androtetris/RecordDataHelper.java
+++ app/src/main/java/com/example/androtetris/RecordDataHelper.java
@@ -1,6 +1,25 @@
-//===============================================
+//Copyright 2021 Andrey S. Ionisyan (anserion@gmail.com, asion@mail.ru)
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+//====================================================
+// Simple tetris game with SQLite and openweather REST
+//====================================================
 // SQLite interaction routines for ITSchool pleasure
-//===============================================
+// intentionally simplified for school use in future
+// database has only one row with current game record
+// demonstarted methods of creating, reading and updating DB
+//====================================================
 package com.example.androtetris;
 
 import android.content.ContentValues;
--- app/src/main/java/com/example/androtetris/WeatherRestHelper.java
+++ app/src/main/java/com/example/androtetris/WeatherRestHelper.java
@@ -1,9 +1,27 @@
-//===============================================
-// REST interaction routines for ITSchool pleasure
-//===============================================
-package com.example.androtetris;
+//Copyright 2021 Andrey S. Ionisyan (anserion@gmail.com, asion@mail.ru)
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 
-import com.google.gson.annotations.SerializedName;
+//====================================================
+// Simple tetris game with SQLite and openweather REST
+//====================================================
+// REST interaction routines for ITSchool pleasure
+// retrofit API interafce and factory with GSon transform inside
+// query for Stavropol city was compiled.
+// http://api.openweathermap.org/data/2.5/weather?q=Stavropol,Russia&appid=KEY
+// Select another cities interaction was left to shine future
+//====================================================
+package com.example.androtetris;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -11,8 +29,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-//http://api.openweathermap.org/data/2.5/weather?q=Stavropol,Russia&appid=b34fcd2b5d42bc626e170a0d80f7a99f
-//https://proft.me/2017/05/5/poluchenie-prognoza-pogody-android-retrofit/
 public class WeatherRestHelper {
     public static String CITY_NAME = "Stavropol,Russia";
     public static String KEY = "b34fcd2b5d42bc626e170a0d80f7a99f";
